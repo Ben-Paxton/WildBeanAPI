5a368bd27e2b4deacb7381e35440d46fd34f4c0d
Ben-Paxton <52067258+Ben-Paxton@users.noreply.github.com>
2023-02-12 17:48:31 +1300

    Initial commit

---
+++ WildBeanAPI/Controllers/CoffeeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
+using WildBeanAPI.Models;
+using WildBeanAPI.Utils;
+
+namespace WildBeanAPI.Controllers
+{
+    [Route("api/coffee")]
+    [ApiController]
+    public class CoffeeController : ControllerBase
+    {
+        // GET: api/Coffee/<brew-coffee>
+        [HttpGet("brew-coffee")]
+        [EnableRateLimiting("ratePolicy")]
+        public IActionResult Get()
+        {
+            var result = StatusCode(404, null);
+
+            try {
+                if (string.Equals(DateTimeProvider.Now.ToString("dd-MM"), "01-04"))
+                {
+                    result = StatusCode(418, null);
+                }
+                else
+                {
+                    result = StatusCode(200, new CoffeeModel { Message = "Your piping hot coffee is ready", Prepared = DateTime.Now.ToString("yyyy-MM-ddTHH:mm:ssK") });
+                }
+            } catch (Exception ex)
+            {
+                result = BadRequest(ex.Message);
+            }
+
+            return result;
+        }
+    }
+}
